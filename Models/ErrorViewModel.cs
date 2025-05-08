5a35ba68c1139092d8094882cef16ec71422fe06
Kocaefe <efekocaefe46@gmail.com>
2025-05-08 14:49:03 +0300

    SIR-1 <proje olusturuldu

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SirketYonetim.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
