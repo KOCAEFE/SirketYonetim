ba1120386b0e072331070a74eeda8469db168568
Kocaefe <efekocaefe46@gmail.com>
2025-05-18 01:20:13 +0300

    Order entity eklendi

---
+++ Entities/Order.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SirketYonetim.Entities
+{
+    public class Order
+    {
+        public string OrderName { get; set; }
+
+        public string Description { get; set; }
+
+        public string Address { get; set; }
+
+        public Guid CustomerId { get; set; }
+
+        
+    }
+}
