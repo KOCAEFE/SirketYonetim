87d0d53e4a55b2a626dcc6afdbf984baf3ea9420
Taner Çam <tanercamm@outlook.com>
2025-05-22 13:41:34 +0300

    SIR-42: Dependency Injection, Service'ler için uygulandı.

--- Program.cs
+++ Program.cs
@@ -13,6 +13,10 @@ using SirketYonetim.Repositories.Concrete.Order;
 using SirketYonetim.Repositories.Concrete.Product;
 using SirketYonetim.Repositories.Abstract.AppUser;
 using SirketYonetim.Repositories.Concrete.AppUser;
+using SirketYonetim.Services.Abstract;
+using SirketYonetim.Services.Concrete;
+using SirketYonetim.Repositories.Abstract.Employee;
+using SirketYonetim.Repositories.Concrete.Employee;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -39,6 +43,9 @@ builder.Services.AddScoped(typeof(IWriteRepository<>), typeof(WriteRepository<>)
 builder.Services.AddScoped<ICustomerReadRepository, CustomerReadRepository>();
 builder.Services.AddScoped<ICustomerWriteRepository, CustomerWriteRepository>();
 
+builder.Services.AddScoped<IEmployeeReadRepository, EmployeeReadRepository>();
+builder.Services.AddScoped<IEmployeeWriteRepository, EmployeeWriteRepository>();
+
 builder.Services.AddScoped<IOrderReadRepository, OrderReadRepository>();
 builder.Services.AddScoped<IOrderWriteRepository, OrderWriteRepository>();
 
@@ -47,6 +54,11 @@ builder.Services.AddScoped<IProductWriteRepository, ProductWriteRepository>();
 
 builder.Services.AddScoped<IAppUserReadRepository, AppUserReadRepository>();
 builder.Services.AddScoped<IAppUserWriteRepository, AppUserWriteRepository>();
+
+builder.Services.AddScoped<ICustomerService, CustomerService>();
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+builder.Services.AddScoped<IOrderService, OrderService>();
+builder.Services.AddScoped<IProductService, ProductService>();
 
 var app = builder.Build();
 
