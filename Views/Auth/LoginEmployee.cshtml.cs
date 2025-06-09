11eb204da522d6a820f653c9efc7575a59bf9e96
mbunyaminaltun <thorendgame20@gmail.com>
2025-06-10 00:54:59 +0300

    SIR-71 : LoginEmployee.cshtml oluşturuldu.

---
+++ Views/Auth/LoginEmployee.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SirketYonetim.Views.Auth
+{
+    public class LoginEmployeeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
